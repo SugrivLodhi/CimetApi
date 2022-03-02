c65ff6938ef7bd712ed1fab3a01d93d0c578b477
Sugriv Lodhi <87061882+SugrivLodhi@users.noreply.github.com>
2022-03-02 16:56:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CimetToken from "./component/CimetToken";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CimetToken />
+    </div>
+  );
+}
